74237ba281f38086e72c104ef92d49f7cdf0b132
Strider <exolivil@gmail.com>
2021-09-09 20:35:05 -0500

    empezando el proyecto

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Isaac Loez on 09/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
